44f87fac1a24fe681203dccb79f7e9eaa52f2e75
SamuelQZQ <928221136@qq.com>
2018-09-16 13:47:10 +0800

    init

---
+++ CppToySolution/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppToySolution
+//
+//  Created by QZQ on 16/09/2018.
+//  Copyright © 2018 QZQ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
