0a129eb55ec3392babb1e57102c0835cfefd8648
SamuelQZQ <928221136@qq.com>
2018-10-08 19:41:18 +0800

    refactor

--- Utility/MemoryPool/MemoryPoolTest.h
+++ Utility/MemoryPool/MemoryPoolTest.h
@@ -12,6 +12,24 @@
 
 void Test()
 {
-    MemoryPool p(1,2);
+    using namespace std;
+    MemoryPool pool(32, 2);
+    
+    void * p1 = pool.Obtain();
+    cout<<p1<<endl;
+    
+    void * p2 = pool.Obtain();
+    cout<<p2<<endl;
+    
+    pool.Return(p1);
+    void * p3 = pool.Obtain();
+    cout<<p3<<endl;
+    
+    pool.Return(p2);
+    void * p4 = pool.Obtain();
+    cout<<p4<<endl;
+    
+    void * p5 = pool.Obtain();
+    cout<<p5<<endl;
 }
 #endif /* MemoryPoolTest_h */
