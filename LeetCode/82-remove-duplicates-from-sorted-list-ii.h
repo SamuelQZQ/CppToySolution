a7b45286d860ba999abd822d61070f35a6909b33
SamuelQZQ <928221136@qq.com>
2018-10-21 15:56:01 +0800

    LeetCode #82

--- LeetCode/82-remove-duplicates-from-sorted-list-ii.h
+++ LeetCode/82-remove-duplicates-from-sorted-list-ii.h
@@ -16,21 +16,43 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// FUCK!
+// 去循环一个在循环体内变的变量，不如循环一个不变的！
+// 1. 链表问题边界情况包括：1.删的或增加的是 头或尾结点
+// 2. 链表问题注意维护好 head、tail
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(NULL == head) return head;
         
-        ListNode *p = head;
-        while(p != NULL)
+        ListNode *shead = new ListNode(-1);
+        shead->next = head;
+        
+        ListNode *q = shead, *p;
+        while(NULL != q && NULL != q->next)
         {
-            while(NULL != p->next && p->next->val == p->val)
+            p = q->next;
+            while(NULL != p && NULL != p->next && p->next->val == p->val)
             {
-                delete p->next;
-                p->next = p->next->next;
+                while(NULL != p->next && p->next->val == p->val)
+                {
+                    ListNode *temp = p->next;
+                    p->next = p->next->next;
+                    delete temp;
+                }
+                
+                q->next = q->next->next;
+                delete p;
+                p = q->next;
             }
-            p = p->next;
+            
+            q = q->next;
+            
         }
+        
+        head = shead->next;
+        delete shead;
         return head;
     }
 };
